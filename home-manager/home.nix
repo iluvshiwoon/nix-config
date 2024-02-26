3107f2f155f840e33e9570f4a636897a56dc4600
iluvshiwoon <sntcillian@gmail.com>
2024-02-26 15:47:02 +0100

    valgrind

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -111,6 +111,7 @@
     unzip
     wget
     xdg-utils
+    valgrind
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
