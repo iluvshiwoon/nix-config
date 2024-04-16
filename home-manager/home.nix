8a32e445b0d2c259eaaffcd18629bdb6040872f2
iluvshiwoon <sntcillian@gmail.com>
2024-04-16 11:18:23 +0200

    Add emacs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -27,6 +27,7 @@
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
+      (import inputs.emacs-overlay)
 
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
--- home-manager/programs/emacs/emacs.nix
+++ home-manager/programs/emacs/emacs.nix
@@ -1,7 +1,18 @@
-{ config, pkgs, inputs,... }: {
-  nixpkgs.overlays = [ (import inputs.emacs-overlay) ];
-  programs.emacs = {
-    enable = true;
+{pkgs, inputs, config, ... }:
+let 
+  my_emacs = pkgs.emacsWithPackagesFromUsePackage {
+    config = ./init.el;
+    defaultInitFile = true;
     package = pkgs.emacs29;
+    alwaysEnsure = true;
+    alwaysTangle = true;
   };
+in
+{
+  services.emacs = {
+    enable = true;
+    package = my_emacs;
+  };
+
+  home.file.".emacs.d/init.el".source = ./init.el;
 }
