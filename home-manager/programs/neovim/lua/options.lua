f6be399148ee03895224b171cd6ad9d000194597
iluvshiwoon <sntcillian@gmail.com>
2024-04-24 09:18:31 +0200

    Update nvim config

---
+++ home-manager/programs/neovim/lua/options.lua
@@ -0,0 +1,58 @@
+-- leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- number 
+vim.o.nu = true
+vim.o.relativenumber = true
+
+-- tab
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- indent
+vim.o.smartindent = false -- disabled for treesitter indent
+
+-- line
+vim.o.wrap = false
+
+-- search
+vim.o.ignorecase = true
+vim.o.smartcase = true -- assume case sensitive when using mixed case
+
+-- random
+vim.o.cursorline = true
+vim.o.backspace = "indent,eol,start"
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.undofile = true
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+vim.o.scrolloff = 8
+vim.o.signcolumn = 'yes'
+vim.o.colorcolumn = "80"
+
+vim.o.updatetime = 300
+
+vim.o.termguicolors = true
+
+vim.o.mouse = 'a'
+
+-- netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- 42_header
+vim.g.user42 = 'kgriset'
+vim.g.mail42 = 'kgriset@student.42.fr'
+
+-- windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
---
+++ home-manager/programs/neovim/lua/plugins/lsp.lua
@@ -0,0 +1,100 @@
+local lspconfig = require('lspconfig')
+
+local on_attach = function(_, bufnr)
+    local bufmap = function(keys, func)
+        vim.keymap.set('n', keys, func, { buffer = bufnr })
+    end
+
+    bufmap('<leader>r', vim.lsp.buf.rename)
+    bufmap('<leader>a', vim.lsp.buf.code_action)
+
+    bufmap('gd', vim.lsp.buf.definition)
+    bufmap('gD', vim.lsp.buf.declaration)
+    bufmap('gI', vim.lsp.buf.implementation)
+    bufmap('<leader>D', vim.lsp.buf.type_definition)
+
+    bufmap('gr', require('telescope.builtin').lsp_references)
+    bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
+    bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+
+    bufmap('K', vim.lsp.buf.hover)
+
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, {})
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+lspconfig.nil_ls.setup {}
+
+lspconfig.pyright.setup {}
+
+lspconfig.bashls.setup {}
+
+lspconfig.clangd.setup {}
+
+lspconfig.eslint.setup({
+    on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            commmand = "EslintFixAll",
+        })
+    end,
+    cmd = { "/nix/store/fc1bdvr36krhzq6ildabwlypr6b1gknc-eslint_d-13.0.0/bin/eslint_d" }
+})
+
+lspconfig.tsserver.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    init_options = {
+        preferences = {
+            disableSuggestions = true,
+        }
+    }
+}
+
+
+lspconfig.lua_ls.setup({
+})
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
+
+require('neodev').setup({})
